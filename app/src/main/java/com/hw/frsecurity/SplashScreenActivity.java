5a0f58852ebed2278f085865bf65c23a6ee64b19
Andrew <andrewtgg@yahoo.com>
2019-11-12 19:24:12 -0800

    Splash screen created

---
+++ app/src/main/java/com/hw/frsecurity/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.hw.frsecurity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(getApplicationContext(),
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
+
