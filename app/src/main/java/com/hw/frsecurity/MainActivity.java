31880ef50d74bdf2f9d18246ad873a53966780d3
Andrew <andrewtgg@yahoo.com>
2019-11-13 17:47:10 -0800

    new background, repositioned icon

--- app/src/main/java/com/hw/frsecurity/MainActivity.java
+++ app/src/main/java/com/hw/frsecurity/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //<< this
         setContentView(R.layout.activity_main);
         findViewById(R.id.button_main_cam).setOnClickListener(new View.OnClickListener() {
             @Override
