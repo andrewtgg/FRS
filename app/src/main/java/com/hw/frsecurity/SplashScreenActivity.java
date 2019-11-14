b2f9b46f3436d52db52b02e13a0403199bed1c4d
Josh Tai <jitai@ucsc.edu>
2019-11-13 16:56:01 -0800

    splash screen

--- app/src/main/java/com/hw/frsecurity/SplashScreenActivity.java
+++ app/src/main/java/com/hw/frsecurity/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,6 +13,22 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+       /* Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+            }
+        }, 10000);*/
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         Intent intent = new Intent(getApplicationContext(),
                 MainActivity.class);
